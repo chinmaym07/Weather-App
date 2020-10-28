c21d16c86e5c14c3f40caf8c25d3841949d21b33
chinmaym07 <b418020@iiit-bh.ac.in>
2020-10-29 03:07:15 +0530

    Converted App to PWA

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+serviceWorker.register();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
