45fe473e622e9e86ebc591ae4b15e09d62b29409
chinmaym07 <b418020@iiit-bh.ac.in>
2021-04-17 00:19:27 +0530

    Updated Config

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
-
-import {API_KEY} from './key';
 const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const fetchWeather = async({city,country}) => {
     const {data} = await axios.get(URL, {
         params : {
             q : `${city},${country}`,
-            appid : API_KEY
+            appid : process.env.REACT_APP_API_KEY
         } 
     });
 
